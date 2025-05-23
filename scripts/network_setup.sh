7b71e0e670737f60ca6c0c9287c6f58b1667b102
Gagandeep Singh <g.singh@nxp.com>
2025-05-23 10:10:39 +0530

    add network setup script
    Signed-off-by: Gagandeep Singh <g.singh@nxp.com>

---
+++ scripts/network_setup.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright 2025 NXP
+
+# Usage: ./network_setup.sh <iface1> <ip1> <iface2> <ip2> ...
+# Example: ./network_setup.sh eth0 1.1.1.2 eth1 2.1.1.2 eth2 3.1.1.2
+
+if (( $# % 2 != 0 )); then
+    echo "Error: Arguments must be in pairs of <interface> <IP address>"
+    exit 1
+fi
+
+# Set unlimited locked memory
+ulimit -l unlimited
+
+# Enable IP forwarding
+echo 1 > /proc/sys/net/ipv4/conf/all/forwarding
+
+# Loop through interface-IP pairs
+while (( "$#" )); do
+    IFACE=$1
+    IPADDR=$2
+
+    echo "Configuring $IFACE with IP $IPADDR..."
+
+    # Disable offloading and VLAN features
+    ethtool -K $IFACE gro off gso off tso off rxvlan off txvlan off rxhash off
+    ethtool -A $IFACE rx off tx off autoneg off
+
+    # Assign IP address
+    ip addr add ${IPADDR}/24 dev $IFACE
+
+    # Bring interface up
+    ip link set $IFACE up
+
+    # Shift to next pair
+    shift 2
+done
+
+# Flush and list NAT table
+iptables -t nat -F
+iptables -t nat -L -n
+
+# Allow forwarding of established connections
+iptables -A FORWARD -m state --state RELATED,ESTABLISHED -j ACCEPT
+
+echo "All interfaces configured successfully."
+
